5850b3332f1f72b7a9f284105a865cb9ae299641
verobrian <verongugi11@gmail.com>
2019-02-20 19:27:41 +0300

    created a program to calculate the area of a circle

---
+++ area of triangle.py
@@ -0,0 +1,4 @@
+height= input("enter height")
+base= input("enter base")
+area= int(height)* int(base)/2
+print(area) 
---
+++ circle.py
@@ -0,0 +1,4 @@
+radius= input("enter radius")
+pie= input("enter pie")
+area= float(pie) *float(radius)**2
+print(area)
