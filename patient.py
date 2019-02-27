b890057fa8aed24ba056a833f12d037871cfccdc
verobrian <verongugi11@gmail.com>
2019-02-27 20:47:28 +0300

    program with if and else conditions

--- patient.py
+++ patient.py
@@ -1,5 +1,7 @@
-is_hot=True
+is_hot= False
 if  is_hot:
     print("it's a hot day")
     print("drink enough water")
+else:("it's a cold day")
+print("wear warm clothes")
 print("have a good day swiry")
